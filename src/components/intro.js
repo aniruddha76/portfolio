96749099d2e1e41012d7fcccf86e7f90fd37ae66
aniruddha <aniruddhathakur76@gmail.com>
2023-11-09 14:08:46 +0530

    Portfolio using React

---
+++ src/components/intro.js
@@ -0,0 +1,16 @@
+const section1 = () => {
+    return (
+        <section class="section1" id="section1">
+            <div class="intro">
+                Hi,<br />
+                This is Aniruddha Thakur
+            </div>
+
+            <div class="profile">
+            This will be image
+            </div>
+        </section>
+    );
+}
+
+export default section1;
---
+++ src/components/nav.js
@@ -0,0 +1,22 @@
+const nav = () => {
+    return (
+        <nav class="navbar">
+            <div class="logo">
+            this is logo
+            </div>
+
+            <div class="links">
+                <a href="#">Home</a>
+                <a href="#">Skills</a>
+                <a href="#">Projects</a>
+                <a href="#">Contact Me</a>
+            </div>
+
+            <div class="contact-btn">
+                <button type="submit" id="contact-btn">Contact Me</button>
+            </div>
+        </nav>
+    );
+}
+
+export default nav;
